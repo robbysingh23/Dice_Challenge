576b9ca9b016c276169560b2ce54facfb27d3f12
Robby Singh <kingraj2323@gmail.com>
2021-05-07 19:16:45 -0500

    setting up the next part. added a flag emoji for the final step

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ document.querySelector(".dice .img1").setAttribute("src", image1);
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let image2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".dice .img2").setAttribute("src", image2);
+
+// Change the title to dislay a winner
+// conditionals would be the best choice
+🚩
+document.querySelector("h1").innerHTML(" Player 1 Wins!")
+document.querySelector("h1").innerHTML("Player 2 Wins!")
+document.querySelector("h1").innerHTML("Draw!")
