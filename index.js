493c454548fbfe9162e28e792833b683b9b68bdb
Robby Singh <kingraj2323@gmail.com>
2021-05-09 18:36:12 -0500

    changed a couple of the variable names to increase readibility for users.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Player 1 dice
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let image1 = "images/dice" + randomNumber1 + ".png";
+let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 document.querySelector(".dice .img1").setAttribute("src", image1);
 
 // Player 2 dice
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-let image2 = "images/dice" + randomNumber2 + ".png";
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".dice .img2").setAttribute("src", image2);
 
 // Change the title to dislay a winner
