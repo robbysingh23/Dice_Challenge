0d8300df0e234e000621f76e04d29ff1622bd820
Robby Singh <kingraj2323@gmail.com>
2021-05-09 19:17:57 -0500

    Modified another variable :q

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Player 1 dice
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
-document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img1").setAttribute("src", randomDiceImage1);
 
 // Player 2 dice
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".dice .img2").setAttribute("src", image2);
+document.querySelector(".dice .img2").setAttribute("src", randomDiceImage2);
 
 // Change the title to dislay a winner
 if (randomNumber1 > randomNumber2) {
